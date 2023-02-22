b69fd3736da6f38d5dfddbf0c53cf3628d8b898b
Diego Mero <diegomero_1999@hotmail.com>
2023-02-22 00:42:16 -0500

    Add features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    get 'random_message', to: 'greetings#random_message'
+  end
 end
